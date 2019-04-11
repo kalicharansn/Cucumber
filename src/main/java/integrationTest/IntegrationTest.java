17d2115ed9cdc43bae81f86fed5128bcbffe6590
Kalicharan <snkalicharan@gmail.com>
2019-04-11 21:53:28 +0530

    updated version

--- src/main/java/integrationTest/IntegrationTest.java
+++ src/main/java/integrationTest/IntegrationTest.java
@@ -7,14 +7,14 @@ public class IntegrationTest {
 	@Given("^user opens chrome browser and enters url$")
 	public void user_opens_chrome_browser_and_enters_url() throws Throwable {
 
-		System.out.println("URL entered");
+		System.out.println("URL entered is a valid one");
 		
 	}
 
 	@When("^user enters \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void user_enters_and(String username, String password) throws Throwable {
 		
-		System.out.println(username);
+		System.out.println(username+password);
 		System.out.println(password);
 
 	}
